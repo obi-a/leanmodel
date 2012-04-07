348a23c91c2bbeb9f73e051d788e8137775e756e
obi-a <obioraakubue@yahoo.com>
2012-04-07 01:45:18 -0400

    use require 'leanmodel' not 'lean_model'

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'lean_model'
+require 'leanmodel'
 
 class Test::Unit::TestCase
 end
