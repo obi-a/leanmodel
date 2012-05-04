f022389ab2e3e91743440dbc352f62e804031402
obi-a <obioraakubue@yahoo.com>
2012-05-04 00:47:02 -0400

    Added  framework for database operations

--- lib/lean_model/base.rb
+++ lib/lean_model/base.rb
@@ -5,6 +5,8 @@ module LeanModel
   extend ActiveModel::Translation
   include ActiveModel::Validations
   include ActiveModel::AttributeMethods
+
+  attr_accessor :database_token
   
   class_attribute :_attributes
   self._attributes = []
@@ -32,6 +34,38 @@ module LeanModel
    false
  end
 
+#database functions
+ def config(token)
+   @database_token = token
+ end
+
+ def token
+  if @database_token.lambda?
+     @database_token.call
+  end
+ end
+
+ def self.all
+  #returns hash of all key/values in the database
+ end
+
+ def self.find(id)
+  #returns a model object with the given ID
+ end
+
+ def save
+   #save this model object to database
+ end
+
+ def update_attributes(hash)
+   #update this model on database
+ end
+
+ def destroy
+   #delete this model from database
+ end
+
+ #end of database functions
  protected 
   def clear_attribute(attribute)
     send("#{attribute}=",nil)
